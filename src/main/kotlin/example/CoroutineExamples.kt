e266b88cf69a240ae600e7c9ce1df363b66b32ee
Dmitriy Dubson <ddubson@pivotal.io>
2018-11-01 09:09:19 -0400

    Add a more illustrious example of a coroutine

--- src/main/kotlin/example/CoroutineExamples.kt
+++ src/main/kotlin/example/CoroutineExamples.kt
@@ -1,14 +1,33 @@
 package example
 
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-suspend fun main(args: Array<String>) = coroutineScope {
-    launch {
-        Thread.sleep(1000)
-        println("Hello")
+fun main(args: Array<String>) = runBlocking { // Waits until all coroutines are finished inside of it
+    serviceFunction()
+}
+
+suspend fun serviceFunction() {
+    coroutineScope {
+        val first = launch {
+            (1..1000).forEach {
+                delay(10)
+                print("All Work and No Play ")
+                if (it % 4 == 0) println()
+            }
+        }
+
+        val second = launch {
+            (1..1000).forEach {
+                delay(10)
+                print("Makes Jack a Dull Boy! ")
+                if (it % 4 == 0) println()
+            }
+        }
+
+        first.join() // Wait until first coroutine is finished
+        second.join() // Wait until second coroutine is finished
     }
-
-    Thread.sleep(3000)
-    println("Done")
 }
